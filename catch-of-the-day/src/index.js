0b8eb7856e37f402eaf8c27ae15d903c1d6893e2
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-02 18:04:38 +0100

    setup

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class StorePicker extends Component {
+  render () {
+    return (
+      <p>Hello</p>
+    )
+  }
+}
+
+const main = document.getElementById('main');
+
+render (<StorePicker/>, main)
